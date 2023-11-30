2222b8269769828fb049c9b139cfad1b5d3ca8d5
eogbadu <70936452+eogbadu@users.noreply.github.com>
2023-11-30 11:53:27 -0500

    Update get_frames.py

--- get_frames.py
+++ get_frames.py
@@ -1,13 +1,15 @@
 import cv2
-video = cv2.VideoCapture("assn1.mp4")
-nbr_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-fps = video.get(cv2.CAP_PROP_FPS)
 
-timestamp = 2.75
-frame_nbr = timestamp * fps
+def GetFrames ( time, file_name, img_file):
+    video = cv2.VideoCapture(file_name)
+    nbr_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = video.get(cv2.CAP_PROP_FPS)
+
+    timestamp = time
+    frame_nbr = timestamp * fps
 
 
-video.set(1, frame_nbr)
+    video.set(1, frame_nbr)
 
-success, frame = video.read()
-cv2.imwrite(f'Frame at {timestamp}.jpg', frame)
+    success, frame = video.read()
+    cv2.imwrite(img_file, frame)

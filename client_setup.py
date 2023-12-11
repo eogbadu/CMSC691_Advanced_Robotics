e81caf7dc033da75b34c81333b8253b69b8f3074
zjmarg <76889896+zjmarg@users.noreply.github.com>
2023-12-11 17:20:42 -0500

    updated get client

--- client_setup.py
+++ client_setup.py
@@ -1,5 +1,6 @@
 import boto3
 from mturk.config import access_id, secret_key, my_region_name
+from botocore.exceptions import NoCredentialsError
 
 region_name = my_region_name
 aws_access_key_id = access_id
@@ -8,26 +9,17 @@ ENDPOINT_URL = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 # Uncomment this line to use in production
 #ENDPOINT_URL = 'https://mturk-requester.us-east-1.amazonaws.com'
 
-from boto3 import Session
-from botocore.exceptions import NoCredentialsError
-
 def setup_mturk_client():
     try:
-        # Replace these with your AWS credentials
-        aws_access_key_id = 'YOUR_ACCESS_KEY'
-        aws_secret_access_key = 'YOUR_SECRET_KEY'
-        aws_region_name = 'YOUR_AWS_REGION'
-
-        # Create a session using your credentials
-        session = Session(aws_access_key_id=aws_access_key_id,
-                            aws_secret_access_key=aws_secret_access_key,
-                            region_name=aws_region_name)
-
-        # Create an MTurk client and assign it to the class attribute
-        mturk_client = session.client('mturk')
-        return mturk_client
-
-        # Add your further actions here using self.mturk_client
+        client = boto3.client(
+        'mturk',
+        endpoint_url=ENDPOINT_URL,
+        region_name=region_name,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        )
+        
+        return client
 
     except NoCredentialsError:
         print("Credentials not found or invalid.")

bcdd018c878f576b02eaec363655d9d15a3c1d44
zjmarg <76889896+zjmarg@users.noreply.github.com>
2023-12-13 11:14:36 -0500

    modified graphs

--- analyze_results.py
+++ analyze_results.py
@@ -93,9 +93,9 @@ def by_image_analyze(mturk_results):
     ax.set_ylim(0, 100)
 
     # Add sample size labels at the bottom of the bars
-    for i, bar in enumerate(bars):
-        ax.text(bar.get_x() + bar.get_width() / 2, 0, f'n={sample_sizes[i]}', 
-                ha='center', va='bottom', fontsize=12, color='black')
+    #for i, bar in enumerate(bars):
+    #    ax.text(bar.get_x() + bar.get_width() / 2, 0, f'n={sample_sizes[i]}', 
+    #            ha='center', va='bottom', fontsize=12, color='black')
         
     # Add significance labels to the bars
     if is_significant:
@@ -143,8 +143,7 @@ def by_category_analyze(mturk_results):
         sample_sizes_without_image.append(total_without_image)
         errors_with_image.append(calculate_standard_error(matches_with_image, total_with_image))
         errors_without_image.append(calculate_standard_error(matches_without_image, total_without_image))
-
-        is_significant = False
+        significance = []
 
         try:
             # Perform a t-test to check for significance between the two categories
@@ -171,8 +170,12 @@ def by_category_analyze(mturk_results):
     x = np.arange(len(categories))
     width = 0.35
 
-    plt.bar(x, percentages_with_image, width, label='With Image', alpha=0.7, yerr=errors_with_image, capsize=5)
-    plt.bar(x + width, percentages_without_image, width, label='Without Image', alpha=0.7, yerr=errors_without_image, capsize=5)
+    # Plotting bars for 'with image' and 'without image' with error bars
+    bar1 = plt.bar(x - width/2, percentages_with_image, width, label='With Image', yerr=errors_with_image, capsize=5)
+    bar2 = plt.bar(x + width/2, percentages_without_image, width, label='Without Image', yerr=errors_without_image, capsize=5)
+
+    # Set y-axis limits to range from 0 to 100, with some padding
+    #plt.ylim(0, 110)  # Adjusted to provide some space for labels
 
     plt.xlabel('Control Signal Categories')
     plt.ylabel('Percentage of Matches')
@@ -180,17 +183,65 @@ def by_category_analyze(mturk_results):
     plt.xticks([i + width / 2 for i in x], categories, rotation=45, ha='right')
     plt.legend()
 
-    # Add sample size labels to the bars
-    for i in x:
-        plt.text(i, percentages_with_image[i] + 1, f'n={sample_sizes_with_image[i]}', ha='center', va='bottom')
-        plt.text(i + width, percentages_without_image[i] + 1, f'n={sample_sizes_without_image[i]}', ha='center', va='bottom')
+    # Increase the top margin of the plot to have more space for p-values and asterisks
+    plt.subplots_adjust(top=0.85)
 
-        if significance[i]:
-            plt.text(i + width / 2, max(percentages_with_image[i], percentages_without_image[i]) + 2, "*", ha='center', fontsize=12, color='black')
+    # Set the y-axis limit higher to accommodate the significance markers
+    plt.ylim(0, max(max(percentages_with_image), max(percentages_without_image)) + 10)  # Adjust as needed for your data
+
+   # Iterate over each category for calculating significance and plotting
+    for i, category in enumerate(categories):
+        
+        group = grouped_by_real_output.get_group(category)
+
+        try:
+            # Perform a t-test to check for significance
+            _, p_value = stats.ttest_ind(
+                group[group['with_image'] == 1]['matching'],
+                group[group['with_image'] == 0]['matching'],
+                equal_var=False  # assuming unequal variance
+            )
+            
+            # Determine if the difference is significant
+            alpha = 0.05
+            is_significant = p_value < alpha
+            
+        except RuntimeWarning as warning:
+            print("RuntimeWarning:", warning)
+            is_significant = False
+        except Exception as e:
+            print("Exception:", e)
+            is_significant = False
+        finally:
+            # Append the result to the significance list
+            significance.append(is_significant)
+
+        # Coordinates for the significance line
+        x1, x2 = i - width/2, i + width/2
+        y, h, col = max(percentages_with_image[i], percentages_without_image[i]) + 1, 1, 'black'  # Increased h for more padding
+
+        # Sample size at the bottom of the bar
+        #bottom_offset = 3  # Adjust this value as needed to position the text below the bars
+        #plt.text(x1, bottom_offset, f'n={sample_sizes_with_image[i]}', ha='center', va='top', color='black')
+        #plt.text(x2, bottom_offset, f'n={sample_sizes_without_image[i]}', ha='center', va='top', color='black')
+
+
+        if is_significant:
+            # Draw the significance line
+            plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
+            
+            # Add asterisk(s) based on the level of significance
+            sig_str = '*' * int(-np.log10(p_value) // 1)  # Single '*', '**', or '***' depending on significance level
+            plt.text((x1+x2)*.5, y+h, sig_str, ha='center', va='bottom', color=col)
+            
+            # Print the p-value above the asterisk
+            plt.text((x1+x2)*.5, y+h*2+3, f'p={p_value:.3f}', ha='center', va='bottom', color=col)
+
 
     # Save the plot as an png
     plt.savefig(f'{ANALYSIS_PATH}/mturk_category_analysis.png')
     plt.show()
+
 
 def calculate_standard_error(matches, total):
     if total <= 0:
@@ -199,7 +250,7 @@ def calculate_standard_error(matches, total):
 
 
 def by_response_analyze(mturk_results):
-    # Group the data by the 'real_output' column
+    # Group the data by the 'submitted_answer' column
     grouped_by_submitted_answer = mturk_results.groupby('submitted_answer')
 
     categories = []
@@ -230,8 +281,7 @@ def by_response_analyze(mturk_results):
         sample_sizes_without_image.append(total_without_image)
         errors_with_image.append(calculate_standard_error(matches_with_image, total_with_image))
         errors_without_image.append(calculate_standard_error(matches_without_image, total_without_image))
-
-        is_significant = False
+        significance = []
 
         try:
             # Perform a t-test to check for significance between the two categories
@@ -258,8 +308,12 @@ def by_response_analyze(mturk_results):
     x = np.arange(len(categories))
     width = 0.35
 
-    plt.bar(x, percentages_with_image, width, label='With Image', alpha=0.7, yerr=errors_with_image, capsize=5)
-    plt.bar(x + width, percentages_without_image, width, label='Without Image', alpha=0.7, yerr=errors_without_image, capsize=5)
+    # Plotting bars for 'with image' and 'without image' with error bars
+    bar1 = plt.bar(x - width/2, percentages_with_image, width, label='With Image', yerr=errors_with_image, capsize=5)
+    bar2 = plt.bar(x + width/2, percentages_without_image, width, label='Without Image', yerr=errors_without_image, capsize=5)
+
+    # Set y-axis limits to range from 0 to 100, with some padding
+    #plt.ylim(0, 110)  # Adjusted to provide some space for labels
 
     plt.xlabel('Submitted Answer Categories')
     plt.ylabel('Percentage of Matches')
@@ -267,16 +321,62 @@ def by_response_analyze(mturk_results):
     plt.xticks([i + width / 2 for i in x], categories, rotation=45, ha='right')
     plt.legend()
 
-    # Set y-axis limits to range from 0 to 100, with some padding
-    plt.ylim(0, 105)  # Adjusted to provide some space for labels
+    # Increase the top margin of the plot to have more space for p-values and asterisks
+    plt.subplots_adjust(top=0.85)
 
-    # Add sample size labels to the bars
-    for i in x:
-        plt.text(i, percentages_with_image[i] + 1, f'n={sample_sizes_with_image[i]}', ha='center', va='bottom')
-        plt.text(i + width, percentages_without_image[i] + 1, f'n={sample_sizes_without_image[i]}', ha='center', va='bottom')
+    # Set the y-axis limit higher to accommodate the significance markers
+    plt.ylim(0, max(max(percentages_with_image), max(percentages_without_image)) + 15)  # Adjust as needed for your data
 
-        if significance[i]:
-            plt.text(i + width / 2, max(percentages_with_image[i], percentages_without_image[i]) + 2, "*", ha='center', fontsize=12, color='black')
+   # Iterate over each category for calculating significance and plotting
+    for i, category in enumerate(categories):
+        
+        group = grouped_by_submitted_answer.get_group(category)
+
+        try:
+            # Perform a t-test to check for significance
+            _, p_value = stats.ttest_ind(
+                group[group['with_image'] == 1]['matching'],
+                group[group['with_image'] == 0]['matching'],
+                equal_var=False  # assuming unequal variance
+            )
+            
+            # Determine if the difference is significant
+            alpha = 0.05
+            is_significant = p_value < alpha
+            
+        except RuntimeWarning as warning:
+            print("RuntimeWarning:", warning)
+            is_significant = False
+        except Exception as e:
+            print("Exception:", e)
+            is_significant = False
+        finally:
+            # Append the result to the significance list
+            significance.append(is_significant)
+
+        # Coordinates for the significance line
+        x1, x2 = i - width/2, i + width/2
+        y = max(percentages_with_image[i], percentages_without_image[i]) + 3  # Top of the highest bar for this category
+        h = 2  # Increase this for more height
+        col = 'black'  # Color of the significance line
+
+        # Sample size at the bottom of the bar
+        #bottom_offset = 6  # Adjust this value as needed to position the text below the bars
+        #plt.text(x1, bottom_offset, f'n={sample_sizes_with_image[i]}', ha='center', va='top', color='black')
+        #plt.text(x2, bottom_offset, f'n={sample_sizes_without_image[i]}', ha='center', va='top', color='black')
+
+
+        if is_significant:
+            # Draw the significance line
+            plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1.5, c=col)
+            
+            # Add asterisk(s) based on the level of significance
+            sig_str = '*' * int(-np.log10(p_value) // 1)  # Single '*', '**', or '***' depending on significance level
+            plt.text((x1+x2)*.5, y+h, sig_str, ha='center', va='bottom', color=col)
+            
+            # Print the p-value above the asterisk
+            plt.text((x1+x2)*.5, y+h*2+3, f'p={p_value:.3f}', ha='center', va='bottom', color=col)
+
 
     # Save the plot as an png
     plt.savefig(f'{ANALYSIS_PATH}/mturk_submitted_answer_analysis.png')
@@ -294,8 +394,8 @@ if __name__ == "__main__":
    mturk_results = read_results()
 
    #Analyze results
-   #by_image_analyze(mturk_results)
+   by_image_analyze(mturk_results)
    by_response_analyze(mturk_results)
    
-   #by_category_analyze(mturk_results)
+   by_category_analyze(mturk_results)
 
